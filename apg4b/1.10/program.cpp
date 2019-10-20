6db45d0c90498156ed69942a7f7a54b130203430
konosumi <36559308+konosumi@users.noreply.github.com>
2019-10-21 02:04:40 +0900

    Create program.cpp

---
+++ apg4b/1.10/program.cpp
@@ -0,0 +1,29 @@
+/*
+キーポイント
+  while文を使うと繰り返し処理ができる
+  条件式が真のとき処理を繰り返す
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  // ここにプログラムを追記
+
+  cout << "A:";
+  int i = 0;
+  while (i < A) {
+    cout << "]";
+  }
+  cout << endl;
+
+  cout << "B:";
+  int j = 0;
+  while (j < B) {
+    cout << "]";
+  }
+  cout << endl;
+}
