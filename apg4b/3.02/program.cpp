11194bbbe9ff64cf446f16dfe596c9d5f6c46562
konosumi <36559308+konosumi@users.noreply.github.com>
2019-10-22 22:08:51 +0900

    Update program.cpp

--- apg4b/3.02/program.cpp
+++ apg4b/3.02/program.cpp
@@ -44,21 +44,119 @@ int main() {
 
 /*
 tuple
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  tuple<int, string, bool> data(1, "hello", true);
+  get<2>(data) = false;
+  cout << get<1>(data) << endl;  // hello
+ 
+  data = make_tuple(2, "WORLD", true);
+ 
+  int a;
+  string s;
+  bool f;
+  tie(a, s, f) = data;
+  cout << a << " " << s << " " << f << endl;  // 2 WORLD 1
+}
 */
 
-    #include <bits/stdc++.h>
-    using namespace std;
-     
-    int main() {
-      tuple<int, string, bool> data(1, "hello", true);
-      get<2>(data) = false;
-      cout << get<1>(data) << endl;  // hello
-     
-      data = make_tuple(2, "WORLD", true);
-     
-      int a;
-      string s;
-      bool f;
-      tie(a, s, f) = data;
-      cout << a << " " << s << " " << f << endl;  // 2 WORLD 1
-    }
+/*
+auto
+初期化を伴って変数を宣言する場合や範囲for文において、型の部分にautoと書くことによって型を省略することができます。
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string concat(string a, string b) {
+  return a + b;
+}
+ 
+int main() {
+  string a = "Hello,";
+  string b = "World";
+  auto ab = concat(a, b);  // string型
+  cout << ab << endl;
+ 
+  vector<int> c = {1, 2, 3};
+  auto d = c;  // vector<int>型
+ 
+  for (auto elem : d) {
+    // elemはint型
+    cout << elem << endl;
+  }
+}
+*/
+
+/*
+ignore
+pairやtupleを分解する際に要らない要素を捨てたい場合、ignoreをtieの引数に渡すことで、対応する位置の値を捨てることができます。
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  pair<int, int> p(3, 5);
+  int right;
+  tie(ignore, right) = p;  // 2番目の値だけ取り出す
+  cout << right << endl; // 5
+ 
+  tuple<int, string, bool> tpl(2, "hello", false);
+  int x;
+  string s;
+  tie(x, s, ignore) = tpl;  // 1番目の値、2番目の値だけ取り出す
+  cout << x << " " << s << endl; // 2 hello
+}
+*/
+
+/*
+型エイリアス
+型エイリアスを用いると型に別の名前をつけることができます。 pairやtupleなど型名が長くなってしまう場合に型エイリアスを使うと便利です。
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  using pii = pair<int, int>;
+  // これ以降 pii という型名はpair<int, int> と同じ意味で使える
+ 
+  pii p;
+  p = make_pair(1, 2);
+  cout << "(" << p.first << ", " << p.second << ")" << endl;
+  p = pii(3, 4);  // 別のpairを作って代入
+  cout << "(" << p.first << ", " << p.second << ")" << endl;
+}
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  typedef pair<int, int> pii;
+  int N;
+  cin >> N;
+  vector<pii> ab(N);
+
+  for (int i = 0; i < N; i++) {
+      cin >> ab.at(i).second >> ab.at(i).first;
+  }
+
+  sort(ab.begin(), ab.end());
+
+/*
+  for (int i = N - 1; i > 0; i--) {
+      if (ab.at(i).second < ab.at(i - 1).second) {
+          swap(ab.at(i), ab.at(i - 1));
+      }
+  }
+  */
+
+  for (int i = 0; i < N; i++) {
+      cout << ab.at(i).second << " " << ab.at(i).first << endl;
+  }
+}
+
+
