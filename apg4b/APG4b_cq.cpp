fab59302109a26f5cfae937f77abbf02f58f57a4
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-04 22:21:00 +0900

    -

---
+++ apg4b/APG4b_cq.cpp
@@ -0,0 +1,33 @@
+/**
+ * EX6 - 電卓をつくろう / 1.06
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_cq
+ */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+ 
+  if (op == "+") {
+    cout << A + B << endl;
+  }
+  // ここにプログラムを追記
+  if (op == "-") {
+    cout << A - B << endl;
+  }
+  if (op == "*") {
+    cout << A * B << endl;
+  }
+  if (op == "/") {
+    if (B == 0) {
+      cout << "error" << endl;
+    } else {
+      cout << A / B << endl;
+    }
+  }
+  if (op == "?" || op == "=" || op == "!") {
+      cout << "error" << endl;
+  }
+}
---
+++ apg4b/APG4b_cr.cpp
@@ -0,0 +1,13 @@
+/**
+ * EX5 - A足すB問題 / 1.05
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_cr
+ */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // ここにプログラムを追記
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+}
