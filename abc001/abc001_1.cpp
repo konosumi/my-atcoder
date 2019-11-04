36143bf05b48179c23645cb37dc85f3858510031
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-04 22:11:09 +0900

    -

---
+++ abc001/abc001_1.cpp
@@ -0,0 +1,12 @@
+/**
+ * A - 積雪深差
+ * https://atcoder.jp/contests/abc001/tasks/abc001_1
+ */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int H1, H2;
+  cin >> H1 >>  H2;
+  cout << H1 - H2 << endl;
+}
