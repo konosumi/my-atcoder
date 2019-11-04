9bb70690f2b003c9eacbdb1dfbdd755d3749850d
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-04 22:16:09 +0900

    -

--- apg4b/APG4b_a.cpp
+++ apg4b/APG4b_a.cpp
@@ -1,6 +1,10 @@
+/**
+ * A - 1.00.はじめに
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_a
+ */
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   cout << "Hello, world!" << endl;
 }
---
+++ apg4b/APG4b_cv.cpp
@@ -0,0 +1,11 @@
+/**
+ * EX1 - コードテストと出力の練習 / 1.01
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_cv
+ */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+}
