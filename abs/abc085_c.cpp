c6fd9b457ad43fd7e4d0d21194c8da753d15c780
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-04 16:12:56 +0900

    白昼夢

---
+++ abs/abc085_c.cpp
@@ -0,0 +1,25 @@
+/**
+ * https://atcoder.jp/contests/abs/tasks/abc085_c
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  //  N 枚入っていて、合計で Y 円だった
+  int N, Y;
+  cin >> N >> Y;
+
+  // N 枚のお札の合計金額が Y 円となることがありえない場合は、-1 -1 -1 と出力せよ。
+  // 10000 円札 x 枚、5000 円札 y 枚、1000 円札 z 枚」として、x、y、z を空白で区切って出力せよ
+  for (int x = 0; x <= N; x++) {
+    for (int y = 0; y <= N - x; y++) {
+      int z = N - x - y;
+      if (10000 * x + 5000 * y + 1000 * z == Y) {
+        cout << x << ' ' << y << ' ' << z << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "-1 -1 -1" << endl;
+}
---
+++ abs/arc065_a.cpp
@@ -0,0 +1,50 @@
+/**
+ * https://atcoder.jp/contests/abs/tasks/arc065_a
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 英小文字からなる文字列 S が与えられます。
+string S;
+
+bool addStr(string T) {
+  // まったく等しい文字列であれば一致とみなす
+  if (T == S) {
+    return true;
+  }
+
+  // 最低5文字以上の開きがないと、文字が追加できない
+  int diff = S.length() - T.length();
+
+  // 作成途中のTが、Sと現時点で乖離していれば一致することは起き得ない
+  /*
+  if (diff > 0 && T != S.substr(0, T.length())) {
+     return false;
+  }*/
+
+  if (diff >= 7 && S.substr(T.length(), 7) == "dreamer") {
+    return addStr(T + "dreamer") || addStr(T + "dream");
+  }
+  if (diff >= 5 && S.substr(T.length(), 5) == "dream") {
+    return addStr(T + "dream");
+  }
+  if (diff >= 6 && S.substr(T.length(), 6) == "eraser") {
+    return addStr(T + "eraser") || addStr(T + "erase");
+  }
+  if (diff >= 5 && S.substr(T.length(), 5) == "erase") {
+    return addStr(T + "erase");
+  }
+
+  return false;
+  // T の末尾に dream dreamer erase eraser のいずれかを追加する。
+  // 各パターンでの一致を検証する
+  // return addStr(T + "dream") || addStr(T + "dreamer") || addStr(T + "erase") || addStr(T + "eraser");
+}
+
+int main() {
+  cin >> S;
+
+  //  Tが空文字列である状態から始める
+  cout << (addStr("") ? "YES" : "NO") << endl;
+}
