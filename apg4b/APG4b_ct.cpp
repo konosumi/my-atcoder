2a6d59cd0ab3b21eeb8a5476598214e23963a84d
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-04 22:18:30 +0900

    -

---
+++ apg4b/APG4b_ct.cpp
@@ -0,0 +1,11 @@
+/**
+ * EX3 - 計算問題 / 1.03
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_ct
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    // 1/2×100×(100+1)
+    cout << 1 * 100 * (100 + 1) / 2 /* ここに式を書く */ << endl;
+}
---
+++ apg4b/APG4b_cu.cpp
@@ -0,0 +1,11 @@
+/**
+ * EX2 - エラーの修正 / 1.02
+ * https://atcoder.jp/contests/APG4b/tasks/APG4b_cu
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+}
