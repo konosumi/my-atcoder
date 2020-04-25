a1fa896740eedb7b84a718bc98a289d568f84233
konosumi <36559308+konosumi@users.noreply.github.com>
2020-04-26 01:39:11 +0900

    Update arc065_a.cpp

--- abs/arc065_a.cpp
+++ abs/arc065_a.cpp
@@ -20,6 +20,64 @@ int main() {
 }
 */
 
+/**
+ * 正攻法で挑んだ際のパターン
+// 英小文字からなる文字列 S が与えられます。
+string S;
+
+bool addStr() {
+  // 文字列が等しくなると空になる
+  if (S == "") {
+    return true;
+  }
+
+  int length = S.length();
+  if (length < 5) {
+     return false;
+  }
+
+  // T の末尾に dream dreamer erase eraser のいずれかを追加する。
+  if (length >= 7 && S.substr(0, 7) == "dreamer") {
+    S = S.substr(7);
+    if (addStr()) {
+      return true;
+    }
+    S = "dreamer" + S;
+  }
+  if (S.substr(0, 5) == "dream") {
+    S = S.substr(5);
+    if (addStr()) {
+      return true;
+    }
+    S = "dream" + S;
+  }
+  if (length >= 6 && S.substr(0, 6) == "eraser") {
+    S = S.substr(6);
+    if (addStr()) {
+      return true;
+    }
+    S = "eraser" + S;
+  }
+  if (S.substr(0, 5) == "erase") {
+    S = S.substr(5);
+    if (addStr()) {
+      return true;
+    }
+    S = "erase" + S;
+  }
+
+  return false;
+}
+
+int main() {
+  cin >> S;
+
+  //  Tが空文字列である状態から始める
+  cout << (addStr() ? "YES" : "NO") << endl;
+}
+*/
+
+// 英小文字からなる文字列 S が与えられます。
 string S;
 
 void checkStr() {
