26f07d7a37f34b90d4b7085bfcf50fcff2bf5405
konosumi <36559308+konosumi@users.noreply.github.com>
2020-05-10 22:41:05 +0900

    ABC167

--- abc167/abc167_c.cpp
+++ abc167/abc167_c.cpp
@@ -4,13 +4,72 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
+
+int N, M, X;
+
+vector<int>CP(14);
+vector<vector<int>> CA(14, vector<int>(14));
+vector<bool>KAU_KAWANAI(14, false);
+
+int calcPrice() {
+    int price = 0;
+    for (int i = 0; i < N; i++) {
+        if (KAU_KAWANAI.at(i)) {
+            price += CP.at(i);
+        }
+    }
+
+// int k = 0;
+    for (int i = 0; i < M; i++) {
+        int syujyukudo = 0;
+        for (int j = 0; j < N; j++) {
+            if (KAU_KAWANAI.at(j)) {
+                syujyukudo += CA.at(j).at(i);
+            }
+        }
+//        k++;
+
+        if (syujyukudo < 10) {
+            return INT_MAX;
+        }
+// cout << i << " " <<  syujyukudo << endl;
+    }
+
+// cout << M << " " <<  k << " " <<  price << endl;
+    return price;
+}
+
+int saiki(int i) {
+    int minPrice = calcPrice();
+    if (i < N) {
+        KAU_KAWANAI.at(i) = true;
+        minPrice = min(minPrice, saiki(i+1));
+
+        KAU_KAWANAI.at(i) = false;
+        minPrice = min(minPrice, saiki(i+1));
+    }
+
+    return minPrice;
+}
  
 int main() {
     // 高橋くんが書店に行くと、N 冊の参考書が売っていました。i 番目の参考書 (1≤i≤N) は Ci 円で売られていて、購入して読むことで、各 j (1≤j≤M) について j 番目のアルゴリズムの理解度が Ai,j 上がります。
     // また、それ以外の方法で理解度を上げることはできません。
-    int N, M, X;
     cin >> N >> M >> X;
 
-    // cout << score << endl;
+    // vector<int>CP(N);
+    // vector<vector<int>> CA(N, vector<int>(M));
+    // vector<vector<int>(M)>CA(N);
+    for (int i = 0; i < N; i++) {
+        // 値段
+        cin >> CP.at(i);
+        for (int j = 0; j < M; j++) {
+            // アルゴリズムの理解度
+            cin >> CA.at(i).at(j);
+        }
+    }
+
+    int minPrice = saiki(0);
+    cout << (minPrice == INT_MAX ? -1 : minPrice) << endl;
 }
  
--- abc167/abc167_d.cpp
+++ abc167/abc167_d.cpp
@@ -12,19 +12,63 @@ int main() {
     cin >> N >> K;
 
     vector<long long>A(N);
-    // long long minA = INT_MAX;
+    // vector<long long>HND(N + 1, 0);
     for (int i = 0; i < N; i++) {
         cin >> A.at(i);
-        // if (A.at(i) < minA) {
-        //     minA = A.at(i);
-        // }
+        // HND.at( A.at(i) )++; 
     }
 
+    // 最多出現の値を求める
+    /*
+    long long maxA = 0;
+    long long maxHND = 0;
+    for (int i = 0; i < N + 1; i++) {
+        if (HND.at(i) > maxHND) {
+            maxHND = HND.at(i);
+            maxA = i;
+        }
+    }
+    */
+
     long long place = 1;
-    for (int i = 0; i < K; i++) {
+    vector<long long>HND(N + 1, 0);
+    long long kaisuu = 0;
+    for (long long i = 1; i <= K; i++) {
         place = A.at(place - 1);
+        kaisuu++;
+        if (HND.at(place) == 0) {
+            HND.at(place) = kaisuu;
+        } else if (HND.at(place) > 0) {
+            long long loopcnt = kaisuu - HND.at(place);
+            i += ((K - i) / loopcnt) * loopcnt;
+            // cout << "koko";
+        }
     }
     cout << place << endl;
+ 
+
+  /*
+    long long place = 1;
+    long long kaisuu = 0;
+    long long maxAkai = 0;
+    long long syokai = 0;
+    for (long long i = 0; i < K; i++) {
+        place = A.at(place - 1);
+        if (place == maxA) {
+            maxAkai++;
+        }
+ 
+        if (maxAkai == 0) {
+            syokai++;
+        } else if (maxAkai == 1) {
+            kaisuu++;
+        } else if (maxAkai == 2) {
+            i = syokai + ((K - syokai) / kaisuu) * kaisuu;
+            maxAkai++;
+        }
+    }
+    cout << place << endl;
+    */
     // cout << score << endl;
 }
  
